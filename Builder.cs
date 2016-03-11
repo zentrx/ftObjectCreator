68aaa4cc153d428e7fcfa471c068291161dabc12
page <paged.csci@gmail.com>
2016-03-11 11:35:49 -0800

    Added Generic Builder
    Added a generic builder, for when I expand this to other types of files.

---
+++ Builder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadTestingPhysicalCreator {
+    class Builder {
+        public static void Main(String[] args) {
+            BoxCreator b = new BoxCreator();
+            b.GenerateBoxes();
+        }
+    }
+}
